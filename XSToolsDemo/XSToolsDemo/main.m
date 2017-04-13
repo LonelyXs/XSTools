326e2a402f1aeb8213204f95645b53e3bd5a9312
范儿 <xs18355330234@163.com>
2017-04-13 11:19:54 +0800

    kjaksjdasjd
    21212121212121

---
+++ XSToolsDemo/XSToolsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XSToolsDemo
+//
+//  Created by 徐帅 on 2017/4/13.
+//  Copyright © 2017年 徐帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
